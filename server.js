257563073722fc6af4b91b5ef10f4c76fc0fb10d
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2018-08-01 23:43:20 +0530

    first commit for this repo

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+const port = 5000 || process.env.PORT;
+
+app.use(cors());
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, '/dist')));
+app.use(bodyParser());
+
+app.get('/', function(req, res) {
+  console.log('request hit...');
+  res.sendFile(path.join(__dirname, 'dist/demo.html'));
+});
+app.listen(port, function() {
+  console.log('Server running on port ', port);
+});
